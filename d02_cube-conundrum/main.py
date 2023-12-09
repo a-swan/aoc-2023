572c6313435c0d270737a4157157b787928677da
alexpswanson <alexpswanson@gmail.com>
2023-12-09 13:04:11 +0200

    d2 part2

--- d02_cube-conundrum/main.py
+++ d02_cube-conundrum/main.py
@@ -6,16 +6,17 @@ def main(gameData, red=12, green=13, blue=14):
         'green': green,
         'blue': blue
     }
-    valid = []
+    valid = 0
     for gameLine in gameData:
         id, games = gameLine.split(":")
         split = games.strip().replace(";","").replace(",","").split(" ")
 
         cubeSet = {'red': 0, 'green': 0, 'blue': 0}
         for i,k in zip(split[0::2], split[1::2]):
-            if cubeSet[k] < i:
-                cubeSet[k] = i
-        valid.append(cubeSet)
+            if cubeSet[k] < int(i):
+                cubeSet[k] = int(i)
+        #valid.append(cubeSet)
+        valid += cubeSet['red'] * cubeSet['green'] * cubeSet['blue']
     print(valid)
 
 if __name__ == "__main__":
