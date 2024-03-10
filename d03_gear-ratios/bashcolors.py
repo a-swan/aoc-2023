12c922bade47cd95c040545fdd50987866af05c8
alexpswanson <alexpswanson@gmail.com>
2024-03-10 14:55:05 +0200

    part2

--- d03_gear-ratios/bashcolors.py
+++
@@ -1,7 +0,0 @@
-BCOLORS = {
-    "HEADER": '\033[95m',
-    "OKGREEN": '\033[92m',
-    "OKYELLOW": '\033[93m',
-    "ENDC": '\033[0m',
-    "UNDERLINE": '\033[4m'
-}
--- d03_gear-ratios/main.py
+++ d03_gear-ratios/main.py
@@ -28,9 +28,43 @@ def part1(schematic):
     print(sum)
 def part2(schematic):
     with open(schematic, 'r') as file:
+        symbols = []
         for lineNumber, line in enumerate(file):
             for lineIndex, char in enumerate(line.strip()):
-                print(char)
+                if char == '*':
+                    symbols += [(r, c, (lineNumber, lineIndex)) for r in range(lineNumber-1, lineNumber+2) for c in range(lineIndex-1, lineIndex+2)]
+        #print(symbols)
+
+        file.seek(0)
+
+        gears = dict()
+        part_reg = r'\d+'
+        for lineNumber, line in enumerate(file):
+            for match in re.finditer(part_reg, line.strip()):
+                #index = [(lineNumber, j, symbol[2]) for j in range(*match.span()) for symbol in symbols if (lineNumber, j) == symbol[:2]]
+                #print(index)
+                #index = []
+                found_gear = []
+                for j in range(*match.span()):
+                    for symbol in symbols:
+
+                        # AND found_gear == False AND match.group() not in gear dictionary
+                        if (lineNumber, j) == symbol[:2]:
+                            #index.append((lineNumber, j, symbol[2]))
+                            
+                            if symbol[2] not in found_gear:
+                                found_gear.append(symbol[2])
+
+                                if gears.get(symbol[2], False):
+                                    gears[symbol[2]].append(int(match.group()))
+                                else:
+                                    gears[symbol[2]] = [int(match.group())]
+
+        sum = 0
+        for g in gears:
+            if len(gears[g]) == 2:
+                sum += (gears[g][0] * gears[g][1])
+        print(sum)
 
 if __name__ == "__main__":
     schematic = sys.argv[1]
